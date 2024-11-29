27eb5884e20087918dc4b53b3d6f6387a3e7fd23
masashikuwahara <brucemclarenp4@gmail.com>
2024-11-29 23:25:55 +0900

    Viewネーム変更

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LastYearView from '@/views/LastYearView.vue'
-import FunnyCatView from '@/views/GameView.vue'
+import GameView from '@/views/GameView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -19,7 +19,7 @@ const router = createRouter({
     {
       path: '/game',
       name: 'game',
-      component: FunnyCatView,
+      component: GameView,
     },
   ],
 })
