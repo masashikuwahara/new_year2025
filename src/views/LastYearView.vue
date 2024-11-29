7aafbc532b428ab3717bebf990b4fc3fbfe1243c
masashikuwahara <brucemclarenp4@gmail.com>
2024-11-30 00:12:54 +0900

    LastYearページ改修

--- src/views/LastYearView.vue
+++ src/views/LastYearView.vue
@@ -1,3 +1,29 @@
 <template>
-  <h1>昨年はこんなところへ行ってきました</h1>
+  <div>
+    <h1>昨年はこんなところへ行ってきました</h1>
+    <ul class="nav nav-tabs">
+      <li class="nav-item" v-for="(tab, index) in tabs" :key="index">
+        <a class="nav-link" :class="{ active: activeTab === index }" @click="activeTab = index">{{ tab.name }}</a>
+      </li>
+    </ul>
+    <div class="tab-content">
+      <div class="tab-pane" v-for="(tab, index) in tabs" :key="index" :class="{ active: activeTab === index }">
+        {{ tab.content }}
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      activeTab: 0,
+      tabs: [
+        { name: '国内旅行', content: '北海道に行って来ました' },
+        { name: '海外旅行', content: 'ハワイに行ってきました' }
+      ]
+    }
+  }
+}
+</script>
