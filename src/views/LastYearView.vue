2f52aaf5dd1731a2e0f787db7198dfb902ea1eed
masashikuwahara <brucemclarenp4@gmail.com>
2024-11-30 18:55:43 +0900

    画像表示実装

--- src/views/LastYearView.vue
+++ src/views/LastYearView.vue
@@ -8,27 +8,29 @@
     </ul>
     <div class="tab-content">
       <div class="tab-pane" v-for="(item, index) in items" :key="index" :class="{ active: activeTab === index }">
-        {{ item.content }}
+        <h3>{{ item.name }} <br></h3> 
+        <img :src="item.image" alt="">
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import moriokaImage from '@/assets/morioka.jpg';
 export default {
   data() {
     return {
       activeTab: 0,
       items: [
-        { name: '盛岡城', content: '盛岡城' },
-        { name: '中尊寺', content: '中尊寺' },
-        { name: '大仙陵古墳', content: '大仙陵古墳' },
-        { name: '東大寺', content: '東大寺' },
-        { name: '奈良公園', content: '奈良公園' },
-        { name: '吉見百穴', content: '吉見百穴' },
-        { name: '白石城', content: '白石城' },
-        { name: '高島城', content: '高島城' },
-        { name: '松本城', content: '松本城' }
+        { name: '盛岡城', image: moriokaImage },
+        { name: '中尊寺',  },
+        { name: '大仙陵古墳',  },
+        { name: '東大寺', },
+        { name: '奈良公園',  },
+        { name: '吉見百穴',  },
+        { name: '白石城',  },
+        { name: '高島城', },
+        { name: '松本城',  }
       ]
     }
   }
@@ -36,8 +38,16 @@ export default {
 </script>
 
 <style>
+img {
+  width: 95%;
+}
+
 .nav-tabs {
   margin-left: 5%;
   margin-right: 5%;
+}
+
+.tab-content {
+  margin-bottom: 50px;
 }
 </style>
