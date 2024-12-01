2c86feac24ea4d3924e66938c7d20e91297f69cf
masashikuwahara <brucemclarenp4@gmail.com>
2024-12-02 00:26:03 +0900

    レイアウト修正
    ロゴ実装、壁紙、レスポンシブ設定

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1>新年おめでとうございます(仮)</h1>
+  <img src="@/assets/logo.png" class="img-fluid" alt="aisatsu">
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/lastYear">LastYear</router-link> |
@@ -17,6 +17,10 @@
   color: #2c3e50;
 }
 
+body {
+  background-image: url(assets/shougatsu.png); 
+}
+
 nav {
   padding: 30px;
 }
@@ -26,6 +30,28 @@ nav a {
 }
 nav a.router-link-exact-active {
   color: #f39c12;
+}
+
+@media screen and (min-width:480px) {
+  #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+  font-size: 25px;
+}
+
+nav a {
+  color: #d35400;
+}
+nav a.router-link-exact-active {
+  color: #f39c12;
+}
 }
 
 </style>
